d1cdf965c0528805cd37ae288fd98bc18e5c81db
Daniel Hillman <hillmandj@gmail.com>
2014-01-08 14:04:09 -0500

    added name/main

--- node.py
+++ node.py
@@ -18,21 +18,21 @@ class Node:
 	def setNext(self, new_ptr):
 		self.ptr = new_ptr
 
+if __name__ == '__main__':
+	ary = [1,2, 3, 4, 5, 6]
 
-ary = [1,2, 3, 4, 5, 6]
+	head_node = Node(ary[0], None)
+	current_node = head_node
+	for i in range(1,len(ary)):
+		temp_node = Node(ary[i], None)
+		current_node.setNext(temp_node)
+		current_node = current_node.getNext()
 
-head_node = Node(ary[0], None)
-current_node = head_node
-for i in range(1,len(ary)):
-	temp_node = Node(ary[i], None)
-	current_node.setNext(temp_node)
-	current_node = current_node.getNext()
-
-#print data
-current_node = head_node
-while current_node != None:
-	print current_node.getData()
-	current_node = current_node.getNext()
+	#print data
+	current_node = head_node
+	while current_node != None:
+		print current_node.getData()
+		current_node = current_node.getNext()
 
 
 
